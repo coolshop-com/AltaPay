069d167f29b11de48d8edd118b8685ec8c778c9b
Mahad Ashraf <>
2021-07-29 18:10:02 +0500

    remove unnecessary print statements & improve code formatting

--- tests/integration/test_merchant_api.py
+++ tests/integration/test_merchant_api.py
@@ -23,17 +23,14 @@ class APITest(unittest.TestCase):
                        url=altapay_url)
 
     def test_create_payment_request(self):
-        print("--- PAYMENT REQUEST ---")
-
         payment = Payment(api=self.api)
-
-        parameters = {
+        params = {
             'terminal': altapay_test_terminal_name,
             'shop_orderid': generate_order_id(),
             'amount': 1.00,
             'currency': 'EUR'
         }
-        self.assertEqual(payment.create(**parameters), True)
+        self.assertEqual(payment.create(**params), True)
         self.assertIn('url', payment)
         self.assertEqual(len(payment.url) > 0, True)
 
@@ -41,7 +38,6 @@ class APITest(unittest.TestCase):
         reservation = Reservation(api=self.api)
         date_today = date.today()
         params = {
-
             'terminal': altapay_test_terminal_name,
             'shop_orderid': generate_order_id(),
             'amount': '25',
@@ -52,17 +48,13 @@ class APITest(unittest.TestCase):
             'cvc': '123'
         }
 
-        print("--- RESERVATION ---")
         self.assertEqual(reservation.create(**params), True)
         self.assertEqual(reservation.success, True)
 
     def test_reservation_capture(self):
-        print("--- CAPTURE ---")
-
         reservation = Reservation(api=self.api)
         date_today = date.today()
         params = {
-
             'terminal': altapay_test_terminal_name,
             'shop_orderid': generate_order_id(),
             'amount': '25',
@@ -74,10 +66,8 @@ class APITest(unittest.TestCase):
         }
 
         reservation.create(**params)
-
         # parse transaction from reservation response object
         transaction = list(reservation.__data__.values())[1]["transaction"]
-
         # find existing transaction on payment gateway by transaction id
         trans = Transaction.find(transaction['transaction_id'], api=self.api)
         # define params for advanced transaction - can be empty
@@ -104,16 +94,13 @@ class APITest(unittest.TestCase):
                 }
             ]
         }
-
         # capture existing transaction with defined params
         self.assertEqual(trans.capture(**transaction_params).result, 'Success')
 
     def test_release_reservation(self):
-        print("--- RELEASE ---")
         reservation = Reservation(api=self.api)
         date_today = date.today()
         params = {
-
             'terminal': altapay_test_terminal_name,
             'shop_orderid': generate_order_id(),
             'amount': '25',
@@ -123,7 +110,6 @@ class APITest(unittest.TestCase):
             'eyear': date_today.year + 1,
             'cvc': '123'
         }
-
         reservation.create(**params)
         # parse transaction from reservation response object
         response = list(reservation.__data__.values())
@@ -140,7 +126,6 @@ class APITest(unittest.TestCase):
         date_today = date.today()
         order_id = generate_order_id()
         params = {
-
             'terminal': altapay_test_terminal_name,
             'shop_orderid': order_id,
             'amount': '25',
@@ -150,13 +135,12 @@ class APITest(unittest.TestCase):
             'eyear': date_today.year + 1,
             'cvc': '123'
         }
-
         reservation.create(**params)
         # parse transaction from reservation response object
         response = list(reservation.__data__.values())
         transaction_id = response[1]["transaction"]["transaction_id"]
-        transaction = Transaction.find(transaction_id, self.api)
 
+        transaction = Transaction.find(transaction_id, self.api)
         transaction_params = {
             'transaction_id': transaction_id,
             'reconciliation_identifier': order_id,
@@ -180,7 +164,6 @@ class APITest(unittest.TestCase):
                 }
             ]
         }
-
         transaction.capture(**transaction_params)
 
         transaction_params = {
@@ -203,17 +186,15 @@ class APITest(unittest.TestCase):
                 }
             ]
         }
+        refund_resp = transaction.refund(**transaction_params)
 
-        callback = transaction.refund(**transaction_params)
-        self.assertEqual(callback.result, 'Success')
+        self.assertEqual(refund_resp.result, 'Success')
 
     def test_charge_subscription(self):
-        print("--- CHARGE SUBSCRIPTION ---")
         reservation = Reservation(api=self.api)
         date_today = date.today()
         order_id = generate_order_id()
         params = {
-
             'terminal': altapay_test_terminal_name,
             'shop_orderid': order_id,
             'amount': '25',
@@ -225,13 +206,12 @@ class APITest(unittest.TestCase):
             'type': 'subscription',
             'agreement_type': 'recurring'
         }
-
         reservation.create(**params)
         # parse transaction from reservation response object
         response = list(reservation.__data__.values())
         transaction_id = response[1]["transaction"]["transaction_id"]
-        transaction = Transaction.find(transaction_id, self.api)
 
+        transaction = Transaction.find(transaction_id, self.api)
         transaction_params = {
             'transaction_id': transaction_id,
             'reconciliation_identifier': order_id,
@@ -239,14 +219,13 @@ class APITest(unittest.TestCase):
             'currency': 'DKK'
         }
         capture_result = transaction.charge_subscription(**transaction_params)
+
         self.assertEqual(capture_result.result, "Success")
 
     def test_reverse_subscription_charge(self):
-        print("--- REVERSE SUBSCRIPTION CHARGE ---")
         reservation = Reservation(api=self.api)
         date_today = date.today()
         params = {
-
             'terminal': altapay_test_terminal_name,
             'shop_orderid': generate_order_id(),
             'amount': '25',
@@ -258,30 +237,26 @@ class APITest(unittest.TestCase):
             'type': 'subscription',
             'agreement_type': 'recurring'
         }
-
         reservation.create(**params)
         # parse transaction from reservation response object
         response = list(reservation.__data__.values())
         transaction_id = response[1]["transaction"]["transaction_id"]
-        transaction = Transaction.find(transaction_id, self.api)
 
+        transaction = Transaction.find(transaction_id, self.api)
         transaction_params = {
             'transaction_id': transaction_id,
             'amount': '21',
             'currency': 'DKK'
         }
-
         resp = transaction.reserve_subscription_charge(**transaction_params)
 
         self.assertEqual(resp.result, "Success")
 
     def test_funding_list(self):
-        print("--- FUNDING LIST ---")
         funding_list = FundingList(api=self.api)
         self.assertIsNotNone(funding_list.fundings)
         for funding in funding_list.fundings:
             self.assertIsInstance(funding, Funding)
-
         while funding_list._current_page < funding_list.number_of_pages - 1:
             funding_list.next_page()
             self.assertGreater(len(funding_list.fundings), 0)
@@ -289,7 +264,6 @@ class APITest(unittest.TestCase):
         self.assertEqual(contract_identifier, 'EmbraceIT')
 
     def test_funding_list_download(self):
-        print("--- FUNDING LIST DOWNLOAD ---")
         funding_list = FundingList(api=self.api)
         funding = funding_list.fundings[-1]
         filepath = funding.download('./')
@@ -299,11 +273,9 @@ class APITest(unittest.TestCase):
         os.remove(filepath)
 
     def test_fraud_check_in_charge(self):
-        print("--- FRAUD CHECK IN CHARGE ---")
         reservation = Reservation(api=self.api)
         date_today = date.today()
         params = {
-
             'terminal': altapay_test_terminal_name,
             'shop_orderid': generate_order_id(),
             'amount': '3.00',
@@ -317,10 +289,10 @@ class APITest(unittest.TestCase):
                 'fraudCheckTest': 'Checkit!'
             }
         }
-
         reservation.create(**params)
         reservation_data = reservation.__data__
         trans = reservation_data['transactions']['transaction']
+
         self.assertEqual(
             trans['payment_infos']['payment_info']['@name'],
             'fraudCheckTest')
@@ -329,15 +301,13 @@ class APITest(unittest.TestCase):
             'Checkit!')
 
     def test_create_simple_invoice_request(self):
-        print("--- SIMPLE INVOICE ---")
-
         invoice = Invoice(api=self.api)
-
-        parameters = {
+        params = {
             'terminal': altapay_invoice_test_terminal_name,
             'shop_orderid': generate_order_id(),
             'amount': 133.33,
             'currency': 'DKK',
+
             'customer_info': {
                 'billing_postal': 9400,
                 'billing_address': 'Address 12',
@@ -345,36 +315,31 @@ class APITest(unittest.TestCase):
             }
         }
 
-        self.assertEqual(invoice.create(**parameters), True)
+        self.assertEqual(invoice.create(**params), True)
 
         invoice_data = invoice.__data__
         trans = invoice_data['transactions']['transaction']
 
-        self.assertEqual(trans['terminal'], parameters['terminal'])
+        self.assertEqual(trans['terminal'], params['terminal'])
         self.assertEqual(
             trans['shop_order_id'],
-            parameters['shop_orderid'])
+            params['shop_orderid'])
         self.assertEqual(
             trans['customer_info']['billing_address']['address'],
-            parameters['customer_info']['billing_address'])
+            params['customer_info']['billing_address'])
         self.assertEqual(
             trans['customer_info']['billing_address']['postal_code'],
-            parameters['customer_info']['billing_postal'])
+            params['customer_info']['billing_postal'])
         self.assertEqual(
             trans['customer_info']['email'],
-            parameters['customer_info']['email'])
-
+            params['customer_info']['email'])
         self.assertEqual(invoice.success, True)
 
     def test_create_complex_invoice_request(self):
-        print("--- COMPLEX INVOICE ---")
-
         invoice = Invoice(api=self.api)
-
         params = {
-
             'terminal': altapay_invoice_test_terminal_name,
-            'shop_orderid':  generate_order_id(),
+            'shop_orderid': generate_order_id(),
             'amount': 666.66,
             'currency': 'DKK',
 
@@ -452,7 +417,6 @@ class APITest(unittest.TestCase):
         self.assertEqual(
             trans['customer_info']['customer_phone'],
             params['customer_info']['customer_phone'])
-
         self.assertEqual(
             trans['customer_info']['billing_address']['firstname'],
             params['customer_info']['billing_firstname'])
@@ -474,7 +438,6 @@ class APITest(unittest.TestCase):
         self.assertEqual(
             trans['customer_info']['billing_address']['address'],
             params['customer_info']['billing_address'])
-
         self.assertEqual(
             trans['customer_info']['shipping_address']['firstname'],
             params['customer_info']['shipping_firstname'])
